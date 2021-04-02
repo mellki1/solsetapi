2849eea13c4ead41ef8044cbf094e019016cf5bd
Melquisedeque <melquisedeque.silva@mitraecp.com>
2021-04-02 10:44:23 -0300

    first commit

---
+++ src/main/java/com/api/solset/SolsetApplication.java
@@ -0,0 +1,13 @@
+package com.api.solset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolsetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/solset/SolsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.solset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolsetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
