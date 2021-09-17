401007db3c85e5143a7c258196b3e7ea96a85437
Melqui <melquisedeque.silva@mitraecp.com>
2021-09-16 22:47:08 -0300

    correção arquivo

--- src/main/java/com/api/solset/service/FireBaseService.java
+++ src/main/java/com/api/solset/service/FireBaseService.java
@@ -3,14 +3,10 @@ package com.api.solset.service;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.UserRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.ResourceBundle;
 
 @Service
